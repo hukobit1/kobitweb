d5e1375ad94be45dd0de5d2e33e6fce0b1e8b1ae
horpeazy <iyamuhope.nosa647@gmail.com>
2023-04-12 04:59:23 +0000

    fix

--- backend/hukobit/settings.py
+++ backend/hukobit/settings.py
@@ -144,7 +144,7 @@ THUMBNAIL_BACKENDS = ['image_cropping.backends.easy_thumbs.EasyThumbnailsBackend
 
 
 STATIC_URL = '/static/'
-STATIC_URL = '/media/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
