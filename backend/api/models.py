e76cae35e7b65596e1281b5d26ba96b5b7a454a5
horpeazy <iyamuhope.nosa647@gmail.com>
2023-04-12 07:27:22 +0000

    fix: models indent error

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from image_cropping.fields import ImageCropField
 from image_cropping import ImageRatioField, ImageCroppingMixin
 from django.urls import reverse
+from django.http import HttpResponseBadRequest, HttpResponse
+from django.conf import settings
 from urllib.parse import urlencode
+from PIL import Image
 import os
 import requests
-import dotenv
+from .utils import crop_image
 
 
-dotenv.load_dotenv()
+
 # Create your models here.
 
 class SliderImage(ImageCroppingMixin, models.Model):
@@ -22,17 +25,7 @@ class SliderImage(ImageCroppingMixin, models.Model):
         try:
             if self.cropping:
                 x1, y1, x2, y2 = map(int, self.cropping.split(','))
-                url = reverse('crop_image')
-                params = {
-                    'image_path': self.image.name,
-                    'cropping': f'{x1},{y1},{x2-x1},{y2-y1}',
-                    'type': 'sliders'
-                }
-                site_name = os.getenv('SITE_NAME')
-                crop_url = f"{site_name}{url}?{urlencode(params)}"
-                response = requests.get(crop_url)
-                if response.status_code == 200:
-                    return response.content.decode()
+                return crop_image(self.image.name, f'{x1},{y1},{x2-x1},{y2-y1}', 'sliders')
             else:
                 return self.image.url
         except Exception as e:
@@ -46,79 +39,56 @@ class Member(ImageCroppingMixin, models.Model):
 	YONETIMKURULU = 'yonetimKurulu'
 	DENETLEMEKURULU = 'denetlemeKurulu'
 	TOPLULUBASKANI = 'toplulukBaskani'
-	
+
 	CATEGORY_CHOICES = [
 		(YONETIMKURULU, 'yonetimKurulu'),
 		(DENETLEMEKURULU, 'denetlemeKurulu'),
 		(TOPLULUBASKANI, 'toplulukBaskani')
 	]
-	
+
+	@property
+	def image_url(self):
+	    try:
+	        if self.cropping:
+	            x1, y1, x2, y2 = map(int, self.cropping.split(','))
+	            return crop_image(self.image.name, f'{x1},{y1},{x2-x1},{y2-y1}', 'members')
+	        else:
+	            return self.image.url
+	    except Exception as e:
+	        print(e)
+
 	name = models.CharField(max_length=255)
 	position = models.CharField(max_length=255)
 	category = models.CharField(max_length=20, choices = CATEGORY_CHOICES)
 	image= models.ImageField(upload_to='members/')
 	cropping = ImageRatioField('image', '200x200', allow_fullsize=True)
 	linkedin_url = models.CharField(max_length=255, blank=True, null=True)
-	
-	
-	@property
-	def image_url(self):
-		try:
-			if self.cropping:
-				x1, y1, x2, y2 = map(int, self.cropping.split(','))
-				url = reverse('crop_image')
-				params = {
-					'image_path': self.image.name,
-					'cropping': f'{x1},{y1},{x2-x1},{y2-y1}',
-					'type': 'sliders'
-				}
-				site_name = os.getenv('SITE_NAME')
-				crop_url = f"{site_name}{url}?{urlencode(params)}"
-				response = requests.get(crop_url)
-				if response.status_code == 200:
-					return response.content.decode()
-			else:
-				return self.image.url
-		except Exception as e:
-			print(e)
-	
+
 	def __str__(self):
 		return self.name
-
-	
 
 class Event(ImageCroppingMixin, models.Model):
-	id = models.CharField(max_length=255, primary_key=True)
-	name = models.CharField(max_length=255)
-	date = models.DateTimeField()
-	about = models.TextField()
-	image = models.ImageField(upload_to='events/')
-	cropping = ImageRatioField('image', '250x250', allow_fullsize=True)
-	
-	
-	@property
-	def image_url(self):
-		try:
-			if self.cropping:
-				x1, y1, x2, y2 = map(int, self.cropping.split(','))
-				url = reverse('crop_image')
-				params = {
-					'image_path': self.image.name,
-					'cropping': f'{x1},{y1},{x2-x1},{y2-y1}',
-					'type': 'sliders'
-				}
-				site_name = os.getenv('SITE_NAME')
-				crop_url = f"{site_name}{url}?{urlencode(params)}"
-				response = requests.get(crop_url)
-				if response.status_code == 200:
-					return response.content.decode()
-			else:
-				return self.image.url
-		except Exception as e:
-			print(e)
-	
-	def __str__(self):
-		return self.name
+    @property
+    def image_url(self):
+	    try:
+	        if self.cropping:
+	            x1, y1, x2, y2 = map(int, self.cropping.split(','))
+	            return crop_image(self.image.name, f'{x1},{y1},{x2-x1},{y2-y1}', 'events')
+	        else:
+	            return self.image.url
+	    except Exception as e:
+	        print(e)
+
+    id = models.CharField(max_length=255, primary_key=True)
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    about = models.TextField()
+    image = models.ImageField(upload_to='events/')
+    cropping = ImageRatioField('image', '250x250', allow_fullsize=True)
+
+    def __str__(self):
+        return self.name
+
 
 class SiteData(models.Model):
 	url = models.CharField(max_length=500)
@@ -129,7 +99,8 @@ class SiteData(models.Model):
 	instagram_url = models.CharField(max_length=255)
 	github_url = models.CharField(max_length=255)
 	discord_url = models.CharField(max_length=255)
-	
-	
+
+
 	def __str__(self):
 		return "Site Data"
+
---
+++ backend/api/utils.py
@@ -0,0 +1,36 @@
+from django.urls import reverse
+from django.http import HttpResponseBadRequest, HttpResponse
+from django.conf import settings
+from urllib.parse import urlencode
+from PIL import Image
+import os
+
+
+def crop_image(image_path, cropping, type):
+    image_full_path = os.path.join(settings.MEDIA_ROOT, image_path)
+    if not os.path.isfile(image_full_path):
+        return HttpResponseBadRequest('image_path is not valid')
+
+    # parse the cropping string
+    try:
+        x, y, width, height= map(int, cropping.split(','))
+    except ValueError:
+        return HttpResponseBadRequest('cropping is not valid')
+
+    # open the image
+    with open(image_full_path, 'rb') as f:
+        image = Image.open(f)
+
+        # crop the image
+        cropped_image = image.crop((x, y, x + width, y + height))
+
+        # create a new file name for the cropped image
+        file_name, ext = os.path.splitext(os.path.basename(image_path))
+        cropped_file_name = f"{file_name}_cropped{ext}"
+
+        # save the cropped image to a temporary file with the new file name
+        temp_file = os.path.join(settings.MEDIA_ROOT, type, cropped_file_name)
+        cropped_image.save(temp_file)
+
+        # return the URL of the cropped image
+        return os.path.join(settings.MEDIA_URL, type , cropped_file_name)
